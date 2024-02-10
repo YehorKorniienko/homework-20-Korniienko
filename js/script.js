c9dce0598f0a2a21616c8e773c243ab0335098c7
Yehor <augortv@gmail.com>
2024-02-10 17:06:00 +0200

    added dark theme

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let ToggleTheme = document.getElementById('ToggleTheme')
+
+
+
+ToggleTheme.onclick = () => {
+   if(document.documentElement.hasAttribute('theme')) {
+       document.documentElement.removeAttribute('theme')
+       ToggleTheme.value = "Темна тема"
+   }
+   else {
+       document.documentElement.setAttribute('theme', 'dark')
+       ToggleTheme.value = "Світла тема"
+   }
+}
